9ec121fadc2c6b788a9f26364a86326c62c81516
bdog711998 <104231228+bdog711998@users.noreply.github.com>
2022-05-13 16:35:49 -0500

    Unit test for RPS

---
+++ test_RPS.py
@@ -0,0 +1,20 @@
+import unittest
+from RPS import *
+
+
+class Test(unittest.TestCase):
+
+    def test_get_user_selection(self):
+
+
+    def test_get_computer_selection(self):
+
+
+    def test_determine_winner(self):
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
